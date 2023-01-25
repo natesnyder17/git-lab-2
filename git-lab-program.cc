2aacc65d6f11c7862778fa27bb0587a9567d8f1b
natesnyder17 <ns127021@ohio.edu>
2023-01-25 17:41:31 -0500

    Hello Git program

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nathaniel Snyder
+ *        Date: January 25th 2023
+ * Description: says Hello Git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
